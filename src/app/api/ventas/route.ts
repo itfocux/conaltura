fef0dff613ad749cc3d4f479914fc649553b362a
itfocux <it@focuxdigital.com>
2026-03-06 11:46:33 -0500

    Cambiando promise.allSettled por for loop

--- src/app/api/ventas/route.ts
+++ src/app/api/ventas/route.ts
@@ -4,27 +4,40 @@ import { updateDealVentaById } from '../../lib/hubspot';
 
 export async function GET() {
   try {
+    // const delay = (ms: any) => new Promise(resolve => setTimeout(resolve, ms));
     const ventas = await getVentas();
 
-    const results = await Promise.allSettled(
-      ventas.map(updateDealVentaById)
-    );
+    // const results = await Promise.allSettled(
+    //   ventas.map(updateDealVentaById)
+    // );
+    const results: any = [];
 
-    const normalized = results.map((r) =>
-      r.status === 'fulfilled'
-        ? r.value
-        : { success: false, error: r.reason }
-    );
+    for (const venta of ventas) {
+      try {
+        const result = await updateDealVentaById(venta);
+        results.push(result);
+      } catch (error) {
+        results.push({ success: false, status: "rejected", reason: error });
+      }
 
-    const successful = normalized.filter((r) => r.success);
-    const failed = normalized.filter((r) => !r.success);
+      // await delay(120); // ~8 requests por segundo
+    }
+
+    // const normalized = results.map((r) =>
+    //   r.success === 'fulfilled'
+    //     ? r.value
+    //     : { success: false, error: r.reason }
+    // );
+
+    const successful = results.filter((r :any) => r.success);
+    const failed = results.filter((r : any) => !r.success);
 
     return NextResponse.json(
       {
-        total: normalized.length,
+        total: results.length,
         successful: successful.length,
         failed: failed.length,
-        results: normalized,
+        results,
       },
       { status: failed.length > 0 ? 207 : 200 } // 207 = Multi-Status (optional)
     );
